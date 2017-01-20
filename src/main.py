202ac4d3e37b1effb48cfbf24b9ee6d4b2fad739
Jan Niklas Hasse <jhasse@bixense.com>
2017-01-20 21:01:40 +0100

    Beitreten zu einem Spiel implementiert

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import json
 import random
 import string
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 from game import Game
@@ -18,11 +18,11 @@ def new_game():
     # Zufällige Game-ID erzeugen um andere Spieler per URL einladen zu können:
     game_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
     games = Game(game_id)
-    return render_template('new.html', game_id=game_id, server_url=request.url_root)
+    return redirect(url_for('join', game_id=game_id))
 
 @app.route("/join/<game_id>")
 def join(game_id):
-    return "blub: {}".format(game_id)
+    return render_template('join.html', game_id=game_id, server_url=request.url_root)
 
 @app.route("/test")
 def test():
