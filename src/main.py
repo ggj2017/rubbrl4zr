bed62b01f86c435bf4967f652ff4bbe340bf6af6
Jan Niklas Hasse <jhasse@bixense.com>
2017-01-20 19:27:00 +0100

    Beispiel für JSON-Ausgabe von Flask

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
+import json
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/test")
+def test():
+    return json.dumps({
+        'schlüssel1': 123,
+        'schlüssel2': 'irgend ein string',
+    })
 
 if __name__ == "__main__":
     app.run()
