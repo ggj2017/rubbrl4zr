fbba814ee5e7301b110828843c4ea0b5ee1fd32e
Jan Niklas Hasse <jhasse@bixense.com>
2017-01-21 18:26:09 +0100

    Renderable in eigene Datei

--- src/static/game.js
+++ src/static/game.js
@@ -136,23 +136,3 @@ class Vector {
         this.y *= val;
     }
 }
-
-class Renderable {
-    constructor(imgPath, pos, degree){
-        var self = this;
-        self._pos = pos;
-        self._degree = degree;
-        self._loaded = false;
-        self._img = new Image();
-        self._img.onload = function() {
-            self._loaded = true;
-        }
-        self._img.src = imgPath;
-    }
-
-    render(ctx) {
-        if(!this._loaded) return;
-        // console.log("rendering", this._img, this._pos)
-        ctx.drawImage(this._img, this._pos.x, this._pos.y);
-    }
-}
